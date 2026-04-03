fca67c35bdc39dac5fc99ec5720259d4c760dbe0
Dev-Caua-Ricken <cauaricken@gmail.com>
2026-04-03 11:09:58 -0300

    finalização da tarefa com ajustes de css e logs

--- 6_EXPRESS/tarefa2/routes/api/index.js
+++ 6_EXPRESS/tarefa2/routes/api/index.js
@@ -1,19 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const path = require('path');
-
+const fs = require('fs');
 
 router.post('/soma', (req, res) => {
     console.log(req.body)
     const n1 = Number(req.body.n1);
     const n2 = Number(req.body.n2);
     const resultado = n1 + n2;
-    console.log(`O resultado da soma é ${resultado}`);
-    res.redirect(`/web/resultado?resultado=${resultado}`);
-});
 
-router.get('/result', (req, res) => {
-    res.json({resultado});
-})
+    const log =  `
+    Soma: ${n1} + ${n2} = ${resultado}
+    Data: ${new Date().toISOString()}
+    ------------------------------\n`;
+
+    fs.appendFile('log.txt', log, (err) => {
+        if (err) {
+            console.error('Erro ao escrever no arquivo de log:', err);
+        }
+    })
+
+    res.redirect(`/web/resultado?value=${resultado}`);
+});
 
 module.exports = router;

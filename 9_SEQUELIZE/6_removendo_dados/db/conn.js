9d44053fe9bf0f91e8a9f6985ae1dca480d37997
Dev-Caua-Ricken <cauaricken@gmail.com>
2026-05-09 15:58:29 -0300

    editando dados no banco com sequelize

---
+++ 9_SEQUELIZE/6_removendo_dados/db/conn.js
@@ -0,0 +1,9 @@
+const {Sequelize} = require('sequelize');
+
+const sequelize = new Sequelize('nodesequelize', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql'
+})
+
+module.exports = sequelize;
+
